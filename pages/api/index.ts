5b852b2380f39e9c618ad6eaf3746a3627bd0da3
codersharon <124261315+codersharon@users.noreply.github.com>
2023-04-06 11:08:16 +0000

    .

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -7,5 +7,5 @@ type Data = {
 }
 
 export default function handler( req: NextApiRequest, res: NextApiResponse<Data> ) {
-      res.status(200).json({ name: "Jhon Doe" })
+      res.status(200).json({ name: "Jhon Doe" });
 }
